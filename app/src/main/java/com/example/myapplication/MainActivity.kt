b1de31f4d26aeecfe814068f857bececa0a62dbf
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-01 14:19:39 +0300

    Extracted string to resource

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, getString(R.string.action_message), Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
